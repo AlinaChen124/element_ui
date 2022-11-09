37da2cbe45d4dfec2517aa3e3811813d892d8280
we452366 <996925662@qq.com>
2022-11-09 15:37:28 +0800

    style: 修改table的样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <router-view>
-  </router-view>
+  <router-view></router-view>
 </template>
 
 <script>
@@ -11,7 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +18,24 @@ export default {
   /* text-align: center; */
   color: #2c3e50;
   /* margin-top: 60px; */
+}
+
+$table-border-color: #999;
+
+table {
+  border: 1px solid $table-border-color; /*设置边框粗细，实线，颜色*/
+  border-collapse: collapse; /*边框重叠，否则你会看到双实线*/
+}
+
+th {
+  border: 1px solid $table-border-color;
+  background-color: #ececec;
+  color: black;
+  font-weight: bold; /*因为是标题栏，加粗显示*/
+}
+
+td {
+  border: 1px solid $table-border-color;
+  color: #333;
 }
 </style>
